684c46b01c77e4589fee76a182bde2ed962af5ae
Jessica <jesli@g.hmc.edu>
2026-09-22 03:40:01 -0700

    IT WORKSS
    keypad works, but further improvements could be made so first press wont double update if other buttons are pressed and then released before the first original pressed button

--- fpga/e155_lab3_jl/source/impl_1/keypress_fsm.sv
+++ fpga/e155_lab3_jl/source/impl_1/keypress_fsm.sv
@@ -27,7 +27,7 @@ module keypress_fsm(
 	// Scanner (row exerter)
 	logic nrst_scan;
 	logic en_scan;
-	scanner #(.WIDTH(20), .MAX_COUNT(524_289)) scanning(.int_osc (clk), .nreset (nrst_scan), .enable (en_scan), .rows (row_exert));
+	scanner #(.WIDTH(25), .MAX_COUNT(320000)) scanning(.int_osc (clk), .nreset (nrst_scan), .enable (en_scan), .rows (row_exert));
 	
 	
 	// keypress fsm
@@ -39,12 +39,24 @@ module keypress_fsm(
 		case (state)
 			SCAN: nextstate = (one_key) ? PRESS : SCAN; //& d_en
 			PRESS: nextstate = HOLD;
-			HOLD: nextstate = (~c_sync[col_index]) ? HOLD : SCAN; // CHECK SAME KEY [TO BE IMPLEMENTED]             
+			HOLD: nextstate = (~one_key) ? SCAN : HOLD; // CHECK SAME KEY [TO BE IMPLEMENTED]             
 			default: nextstate = SCAN;
 		endcase
 		
 	assign nrst_scan = nrst;
-	assign en_scan = (state == SCAN); // moment one key and debounce are true --> press state stops scan rotation and freezes at row of detection
+	assign en_scan = en;
+	//assign en_scan = (state == SCAN); // moment one key and debounce are true --> press state stops scan rotation and freezes at row of detection
+
+
+
+	logic [3:0] d_write;
+	
+	always_ff @(posedge clk, posedge ~nrst) begin
+		if (~nrst)
+			d_write <= 4'h0;
+		else
+			d_write <= key_next;
+	end
 	
 	always_ff @(posedge clk, posedge ~nrst)
 		if (~nrst) begin
@@ -57,13 +69,16 @@ module keypress_fsm(
 				db_led[0] <= 0;
 				db_led[2] <= 0;
 				d1 <= d0;
-				d0 <= key_next; // combinational decode of {rows, cols}
+				d0 <= d_write; // combinational decode of {rows, cols}
 			end
 			else if (state == SCAN)begin
 				db_led[0] <= 1'b1;
 				db_led[1] <= 0;
 				db_led[2] <= 0; end
-			else db_led[2] <= 1'b1;
+			else begin
+				db_led[2] <= 1'b1;
+				db_led[1:0] <= 2'b00;
+			end
 		end
 
 endmodule
