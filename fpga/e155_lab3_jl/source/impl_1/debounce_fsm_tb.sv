8e0b28bd035321ec2d7675ccfbffcce75dfb6e89
Jessica <jesli@g.hmc.edu>
2026-09-23 12:50:59 -0700

    THREE HOURS LEFT

--- fpga/e155_lab3_jl/source/impl_1/debounce_fsm_tb.sv
+++ fpga/e155_lab3_jl/source/impl_1/debounce_fsm_tb.sv
@@ -8,12 +8,9 @@
 `timescale 1 ns/1 ns
 
 module debounce_fsm_tb();
-  logic           osc, enable, nreset;    
-  logic [15:0]    key;
-  logic           d_en;  
-
-
-// typedef enum logic [1:0] {IDLE, WAIT, PRESSED} statetype;
+	logic           osc, enable, nreset;    
+	logic [15:0]    key;
+	logic           d_en;  
 
     debounce_fsm dut (
 		.keymap(key),
@@ -24,7 +21,7 @@ module debounce_fsm_tb();
     );
 
   // generate clock
-  always begin
+	always begin
 		osc = 0;
 		#10;
 		osc = 1;
@@ -32,26 +29,23 @@ module debounce_fsm_tb();
 	end
 
   // apply stimuli and check outputs
-  initial begin
-    nreset = 0;
-    #22 nreset = 1;
-	
-	
-
-    // a full clock cycle (#10)
-    // Test moving from IDLE to WAIT
-        // setup inputs
-		key = 16'b1;
-	#20;			
+	initial begin
+		enable = 1;
+		nreset = 0;
 		key = 16'b0;
-	#20; // now it should go back to IDLE
+		#22 nreset = 1;
 		
-	#20;
-		key = 16'b11;
-	#12_000_000;
-		
-       
+		#10_500_000;
 
-    #1000 $stop;
-  end
+		key = 16'b1;
+		#10_485_780;			
+		key = 16'b0;
+		#10_485_780; // now it should go back to IDLE
+		key = 16'b1;
+		#12_000_000;
+		#10_485_780;
+		nreset = 0;
+		#10_485_780;
+		#1000 $stop;
+	end
 endmodule
--- fpga/e155_lab3_jl/source/impl_1/keypress_fsm_tb.sv
+++ fpga/e155_lab3_jl/source/impl_1/keypress_fsm_tb.sv
@@ -1,17 +1,43 @@
-// 
-
+// Jessica Li  |  jesli@g.hmc.edu
+// 09/23/2026
+// This is a test bench for the keypress fsm submodule used in the top-level module of lab 3.
+/* The following tests include:
+	1. Sim Waveforms show diagonal key presses individual and show both no press and multipress reactions 
+*/
 
 `timescale 1 ns/1 ns
 
 module keypress_fsm_tb();
-	// Internal high-speed oscillator, 48 MHz clock generated in FPGA by HSOSC primitive
-	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+	logic           osc, enable, nreset, d_en, press;    
+	logic  [3:0]    key, d0, d1; 
+	logic  [2:0]    db_led;
 
-	// col input synchronizer
-	sync col_synchronizer(.clk(int_osc), .d(col_raw), .q(col_sync)); // synchronize all col inputs
-	// sync row_synchronizer(.clk(int_osc), .d(row_raw), .q(row_sync));  
-	
-	// main keypress fsm
-	keypress_fsm dut(.clk(int_osc), .nrst(nreset), .en(enable), .c_sync(col_sync), .r_sync(row_sync), .d_en,
-	.row_exert(row),.d0,.d1);
+    keypress_fsm dut (.clk(osc), .nrst(nreset), .en(enable),.one_key(press),.key_next(key),.d_en,
+		.d0,.d1,.db_led);
+
+	// generate clock
+	always begin
+		osc = 0; 
+		#5;
+		osc = 1; 
+		#5;
+	end
+
+	initial begin
+    nreset = 0;
+    #22 nreset = 1;
+
+	d_en = 1'b1;
+	press = 1'b1;
+	#10;
+			
+	#10; // now it should stuck in HOLD
+		
+	#1000;
+	press = 0;
+	#1000;
+		
+
+    #1000 $stop;
+  end
 endmodule
